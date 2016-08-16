d7b52c554c331ab8fba2e61f4c008338258de9a4
tops <tops@topss-imac.local>
2016-08-16 14:44:51 +0530

    Initial Commit

---
+++ hideViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hideViewExample
+//
+//  Created by tops on 8/16/16.
+//  Copyright © 2016 tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
